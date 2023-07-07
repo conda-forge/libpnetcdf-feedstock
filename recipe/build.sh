4a219fcf376091da067c8ba91db801ed7689b058
Klaus Zimmermann <klaus.zimmermann@smhi.se>
2023-07-07 11:44:28 +0200

    Try to stay out of the way of compiler configuration

--- recipe/build.sh
+++ recipe/build.sh
@@ -7,10 +7,10 @@ set -xe
 # for cross compiling using openmpi
 export OPAL_PREFIX=$PREFIX
 
-export MPICC=mpicc
-export MPICXX=mpicxx
-export MPIF77=mpifort
-export MPIF90=mpifort
+# export MPICC=mpicc
+# export MPICXX=mpicxx
+# export MPIF77=mpifort
+# export MPIF90=mpifort
 
 ./configure --prefix=${PREFIX} \
     --with-mpi=${PREFIX} \
