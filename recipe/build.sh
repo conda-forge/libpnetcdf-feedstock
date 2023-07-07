830de12385dc14832631578d06212602a768772d
Klaus Zimmermann <klaus.zimmermann@smhi.se>
2023-07-07 10:20:52 +0200

    Try a simplified approach for Openmpi

--- recipe/build.sh
+++ recipe/build.sh
@@ -4,16 +4,8 @@ cp $BUILD_PREFIX/share/gnuconfig/config.* ./scripts
 
 set -xe
 
-if [[ ! -z "$mpi" && "$mpi" != "nompi" ]]; then
-  export OMPI_MCA_rmaps_base_oversubscribe=yes
-  export OMPI_MCA_btl=self,tcp
-  export OMPI_MCA_plm=isolated
-  export OMPI_MCA_rmaps_base_oversubscribe=yes
-  export OMPI_MCA_btl_vader_single_copy_mechanism=none
-  mpiexec="mpiexec --allow-run-as-root"
-  # for cross compiling using openmpi
-  export OPAL_PREFIX=$PREFIX
-fi
+# for cross compiling using openmpi
+export OPAL_PREFIX=$PREFIX
 
 export MPICC=mpicc
 export MPICXX=mpicxx
